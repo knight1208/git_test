3bbf65f3aa2da444ea4d848bcad0a5df065d235e
knight1208 <1710316220@qq.com>
2014-12-08 23:03:06 +0800

    首次提交

---
+++ src/day01/Test.java
@@ -0,0 +1,20 @@
+package day01;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Test {
+	public static void main(String[] args) {
+		test();
+	}
+
+	private static void test() {
+		int n=new Random().nextInt(10);
+		System.out.println(n);
+		int[] r=new int[n];
+		for (int i = 0; i < r.length; i++) {
+			r[i]=new Random().nextInt(100);
+		}
+		System.out.println(Arrays.toString(r));
+	}
+}
