1e312513b8e858cfa1f28d12a99d88906c55c170
knight1208 <1710316220@qq.com>
2014-12-08 23:22:34 +0800

    第三次提交

--- src/day01/Test.java
+++ src/day01/Test.java
@@ -7,6 +7,8 @@ public class Test {
 	public static void main(String[] args) {
 		test();
 		System.out.println("*********");
+		System.out.println("*********");
+		System.out.println("*********");
 	}
 
 	private static void test() {
